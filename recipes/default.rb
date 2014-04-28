3481ce76ec95ebda4c994aa20a3bf87bc3b26387
Sliim <sliim@mailoo.org>
2014-04-29 00:22:30 +0200

    Added rubocop tests + fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,46 +15,49 @@
 # limitations under the License.
 #
 
-include_recipe "apt"
+include_recipe 'apt'
 
-case node["mu"]["emacs_package"]
+case node['mu']['emacs_package']
 when false
-  package node["mu"]["emacs_package"] do
+  package node['mu']['emacs_package'] do
     action :install
   end
 end
 
-node["mu"]["packages"].each do |pkg|
+node['mu']['packages'].each do |pkg|
   package pkg do
     action :upgrade
   end
 end
 
-localfile = Chef::Config[:file_cache_path] + "/mu.tar.gz"
+remotefile = 'http://mu0.googlecode.com/files/mu-'
+remotefile << node['mu']['version'] << '.tar.gz'
+localfile = Chef::Config[:file_cache_path] + '/mu.tar.gz'
+
 remote_file localfile do
-  source "http://mu0.googlecode.com/files/mu-" + node["mu"]["version"] + ".tar.gz"
-  mode "0644"
+  source remotefile
+  mode '0644'
 end
 
-directory node["mu"]["build_dir"] do
-  owner "root"
-  group "root"
-  mode "0755"
+directory node['mu']['build_dir'] do
+  owner 'root'
+  group 'root'
+  mode '0755'
   action :create
   recursive true
 end
 
-execute "untar" do
-  cwd node["mu"]["build_dir"]
-  command "tar --strip-components 1 -xzf " + localfile
+execute 'untar' do
+  cwd node['mu']['build_dir']
+  command 'tar --strip-components 1 -xzf ' + localfile
 end
 
-execute "configure and make" do
-  cwd node["mu"]["build_dir"]
-  command "./configure && make"
+execute 'configure and make' do
+  cwd node['mu']['build_dir']
+  command './configure && make'
 end
 
-execute "Install mu" do
-  cwd node["mu"]["build_dir"]
-  command "sudo make install"
+execute 'Install mu' do
+  cwd node['mu']['build_dir']
+  command 'sudo make install'
 end
--- test/integration/mu/serverspec/default_spec.rb
+++ test/integration/mu/serverspec/default_spec.rb
@@ -15,16 +15,16 @@
 # limitations under the License.
 #
 
-require "spec_helper"
+require 'spec_helper'
 
-describe file("/opt/mu") do
+describe file('/opt/mu') do
   it { should be_directory }
 end
 
-describe file("/opt/mu/README") do
+describe file('/opt/mu/README') do
   it { should be_file }
 end
 
-describe file("/usr/local/bin/mu") do
+describe file('/usr/local/bin/mu') do
   it { should be_file }
 end
