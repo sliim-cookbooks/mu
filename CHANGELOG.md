15d6326156eff90246b03565752262e2e15e9c01
Sliim <sliim@mailoo.org>
2014-10-26 02:07:28 +0200

    Bump 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,13 @@ mu CHANGELOG
 
 This file is used to list changes made in each version of the mu cookbook.
 
+0.2.0
+-----
+- Added `compile` recipe for installation from source
+- `default` recipe install from packages if available,
+  else it attempt to include `compile` recipe
+- Added `flags` attribute, a list of flags to pass to configure script
+
 O.1.2
 -----
 - Fixed compilation error
--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ maintainer_email 'sliim@mailoo.org'
 license          'Apache 2.0'
 description      'Installs/Configures mu (maildir-utils)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 
-recipe 'default', 'Install mu from package'
-recipe 'compile', 'Build and install mu from source'
+recipe 'default', 'Install mu from packages'
+recipe 'compile', 'Build and install mu from sources'
 
 depends 'apt'
 supports 'debian'
